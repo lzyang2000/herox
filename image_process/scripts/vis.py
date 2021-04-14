906e9073ae7a755d2a2be36702016436e47afb08
David Shen <dalyshen@berkeley.edu>
2021-04-14 13:50:38 -0700

    draw map first attempt

--- image_process/scripts/vis.py
+++ image_process/scripts/vis.py
@@ -13,6 +13,9 @@ import glob
 import os
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
+import skimage.io as skio
+import skimage as sk
+from skimage import feature
 
 class visualizer:
     def __init__(self,path):
@@ -23,6 +26,10 @@ class visualizer:
         self.images_path = glob.glob(path+'/'+'*.jpg')
         self.coords_path.sort( key=os.path.getmtime)
         self.images_path.sort( key=os.path.getmtime)
+        #self.coords_path.sort()
+        #self.images_path.sort()
+        print(self.coords_path)
+        print(self.images_path)
         self.coords = []
         self.images = []
         last_seen = None
@@ -37,11 +44,56 @@ class visualizer:
                     self.coords.append(coord)
                     self.images.append(cv2.imread(j))
     
+    def draw_wall(self):
+        self.wall = []
+        a = np.array(skio.imread("map3.pgm"))
+        #a = a[::-1]
+        #b = sk.transform.rotate(a, 90, preserve_range=True)
+        b = a
+        b[b>220] = 0
+        e1 = np.array(feature.canny(b,sigma=5), dtype=int)
+        for i in range(len(e1)):
+            for j in range(len(e1[0])):
+                if e1[i][j]:
+                    e1[i][j] = 0
+                else:
+                    e1[i][j] = 255
+        xs, ys = np.where(e1 == 0)
+        #print ys
+        scale = len(a) / 20
+        for i in range(len(xs)):
+            self.wall.append([xs[i] - len(a)/2, ys[i] - len(a)/2])
+        self.wall = np.array(self.wall)
+    
+    def alternative_wall(self):
+        self.wall = []
+        m = np.array(skio.imread("map3.pgm"))
+        fill = m.copy()
+        plt.figure(figsize=(20,10))
+        mask = fill == 254
+        maskup = np.roll(mask, 1, axis = 0)
+        maskright = np.roll(mask, 1, axis = 1)
+        fill[mask] = 0
+        fill[maskup] = 0
+        fill[maskright] = 0
+        fill = filters.gaussian(fill)
+    
+    def raytrace(self, img, x, y, theta):
+        while(y<len(img) and x<len(img)):
+            y += 0.5
+            x += np.tan(theta)*0.5
+            if y>=len(img) or x>=len(img):
+                break
+            if img[int(x)][int(y)] == 0:
+                print(int(x), int(y))
+                break
+            img[int(x)][int(y)] = 125
+    
     
     def process(self):
         self.TwoD_index = []
         for i in self.coords:
-            self.TwoD_index.append([i.position.x,i.position.y])
+            self.TwoD_index.append([i.pose.position.x,i.pose.position.y])
         
         self.TwoD_index = np.array(self.TwoD_index)
         self.fig, self.ax1 = plt.subplots()
@@ -49,10 +101,11 @@ class visualizer:
         self.ax1.set_ylabel('X(m)', picker=True)
         self.ax1.set_xlabel('Y(m)', picker=True)
         line, = self.ax1.plot(self.TwoD_index[:,1],self.TwoD_index[:,0], '-o')
+        #lin2, = self.ax1.plot(self.wall[:,1],self.wall[:,0], '.')
         self.ax1.scatter(self.TwoD_index[:,1],self.TwoD_index[:,0], picker=True)
         # q = self.ax1.quiver(self.TwoD_index[:,1],self.TwoD_index[:,0],u,v)
         self.pick_simple()
-        plt.xlim([-0.5,0.5])
+        #plt.xlim([-0.5,0.5])
         plt.show()
         
     def pick_simple(self):
@@ -72,7 +125,10 @@ class visualizer:
 
 
 def main(args):
-    visualizer('/home/locobot/slam_ws/image_data').process()
+    #visualizer('/home/locobot/slam_ws/image_data').process()
+    v = visualizer('/home/daly/Downloads/1st_floor')
+    v.draw_wall()
+    v.process()
 
 
 if __name__ == '__main__':
