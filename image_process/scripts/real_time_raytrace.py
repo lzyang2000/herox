beb06165b6a55934a8ed81940c8841d9b95068b9
Gary Yang <lzyang@berkeley.edu>
2021-05-02 11:38:37 -0700

    raytrace

--- image_process/scripts/real_time_raytrace.py
+++ image_process/scripts/real_time_raytrace.py
@@ -4,7 +4,7 @@ import rospy
 from std_msgs.msg import Float32
 from sensor_msgs.msg import Image
 from nav_msgs.msg import Odometry
-from nav_msg.msg import OccupancyGrid
+from nav_msgs.msg import OccupancyGrid
 from nav_msgs.msg import Path
 from cv_bridge import CvBridge, CvBridgeError
 from message_filters import TimeSynchronizer, Subscriber, ApproximateTimeSynchronizer
@@ -21,12 +21,14 @@ class tracer():
         rospy.init_node('tracer', anonymous=True)
         self.odom_sub = rospy.Subscriber("image_process/sync_odom",Odometry,callback=self.callback_odom)
         self.map_sub = rospy.Subscriber("map",OccupancyGrid,callback=self.callback_map)
+        self.raytrace_pub = 
         self.tf_listener = tf.TransformListener()
         self.odomList = []
         self.latest_map = None
-
         while not rospy.is_shutdown() and len(self.odomList) and self.latest_map is not None:
             self.raytrace()
+            
+        print('done initialization')
 
 
     def callback_odom(self,data):
@@ -43,6 +45,13 @@ class tracer():
         print('map shape', self.latest_map.shape)
 
     def raytrace(self):
-        pass
+        print('raytrace')
 
 
+if __name__ == '__main__':
+    tracer()
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+        sys.exit(0)
