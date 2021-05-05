226b9527347da1e22c9d367bf74cc19f0fda38e4
Gary Yang <lzyang@berkeley.edu>
2021-05-04 19:39:01 -0700

    before multiprocess

--- image_process/scripts/real_time_raytrace.py
+++ image_process/scripts/real_time_raytrace.py
@@ -30,9 +30,10 @@ class tracer():
         self.odom_sub = rospy.Subscriber("image_process/sync_odom",Odometry,callback=self.callback_odom)
         self.map_sub = rospy.Subscriber("map",OccupancyGrid,callback=self.callback_map)
 
-        self.raytrace_pub = rospy.Publisher("raytrace",OccupancyGrid,queue_size=1,latch=True)
-        topic = 'visualization_marker_array'
-        self.wall_hit_publisher = rospy.Publisher(topic, MarkerArray,queue_size=1,latch=True)
+        self.newMap_pub = rospy.Publisher("wallhit_gridmap",OccupancyGrid,queue_size=1,latch=True)
+        
+        self.wall_hit_publisher = rospy.Publisher('wall_hit', MarkerArray,queue_size=1,latch=True)
+        self.ray_publisher = rospy.Publisher('rays', MarkerArray,queue_size=1,latch=True)
 
         self.tf_listener = tf.TransformListener()
         self.odomList = []
@@ -83,10 +84,13 @@ class tracer():
         map = canvas.astype(np.uint8)
         size = int(np.sqrt(map.shape[0]))
         self.latest_map = map.reshape((size, size))
+        
+        self.latest_map_origin = data.data
         # skio.imsave('test.png', self.latest_map)
 
     def raytrace_on_matplot(self, x, y, theta, step=0.05):
         #print("raytracing starting at", x, y, theta)
+        initial_x, initial_y = x, y
         path_list = []
         hitpoint = None
         xmin = min(self.wall[:, 0])
@@ -97,19 +101,23 @@ class tracer():
         x_step = step * np.cos(theta)
         y_step = step * np.sin(theta)
         
-        
+        length_limit = 2
+        x_limit = x + length_limit*np.cos(theta)
         
         while (x <= xmax and x >= xmin and y <= ymax and y >= ymin):
+            if (x_limit-x)*(x-initial_x) < 0:
+                break
+            
             if np.logical_and(np.isclose(x, self.wall[:, 0], atol=0.05),
                               np.isclose(y, self.wall[:, 1], atol=0.05)).any():
                 hitpoint = [x, y]
                 break
-            #path_list.append([x, y])
+            path_list.append([x, y])
             x += x_step
             y += y_step
-        if hitpoint is None:
-            print('fail to hit wall')
-        #path_list = np.array(path_list)
+        #if hitpoint is None:
+            #print('fail to hit wall')
+        path_list = np.array(path_list)
         return hitpoint, path_list
 
     def euler_from_quaternion(self, x, y, z, w):
@@ -137,6 +145,10 @@ class tracer():
     def get_wall(self):
         self.wall = []
         m = self.latest_map
+        
+        sub_m = m[750:1250, 750:1250]
+        m = sub_m
+        
         fill = m.copy()
         fill = np.transpose(fill, (1, 0))
         fill = fill[:, ::-1]
@@ -153,19 +165,47 @@ class tracer():
         e1[wall] = 1
 
         mask = np.ones(e1.shape)
-        for i in range(len(e1)):
-            for j in range(len(e1[0])):
-                if e1[i][j] and np.sum(e1[i - 2:i + 3, j - 2:j + 3]) >= 4:
-                    mask[i][j] = 0
+        
+        canvas = np.zeros(e1.shape)
+        
+        for i in range(-2, 3):
+            for j in range(-2, 3):
+                tmp = np.roll(e1, i, axis=0)
+                tmp = np.roll(tmp, j, axis=1)
+                canvas += tmp
+                
+                
+        nearwall = canvas >= 6
+        realwall = e1 > 0
+        mask[np.logical_and(nearwall, realwall)] = 0
+        
+
         mask = mask[:, ::-1]
+        
+        wholemask = np.ones(self.latest_map.shape)
+        wholemask[750:1250, 750:1250] = mask
+        mask = wholemask
+        
         xs, ys = np.where(mask == 0)
+        
+        
+        realX, realY = [], []
+        l_m = self.latest_map
+        for x, y in zip(xs, ys):
+            local = l_m[y-2:y+3, x-2:x+3]
+            #print(x, y)
+            if (local == 0).any():
+                realX.append(x)
+                realY.append(y)
+        
+        xs, ys = realX, realY
+            
+        
         scale = 0.05
         for i in range(len(xs)):
             # self.wall.append([(xs[i] - len(fill) / 2) * scale, (ys[i] - len(fill) / 2) * scale])
             self.wall.append([(xs[i]) * scale, (ys[i]) * scale]) #image coord
         self.wall = np.array(self.wall)
-
-
 
     def process(self):
         self.TwoD_index = []
@@ -173,21 +213,34 @@ class tracer():
         self.wall_hits = []
         self.rays = []
         odom = self.odomList[:]
-        print('num odom', len(odom))
         scale = 0.05
+        
+        hit_total = 0
+        
         for i in odom:
             x, y = i.pose.position.x + (self.mapSize/2)*scale, i.pose.position.y + (self.mapSize/2)*scale
             self.TwoD_index.append([x, y])
             theta = self.euler_from_quaternion(i.pose.orientation.x, i.pose.orientation.y, i.pose.orientation.z, i.pose.orientation.w)[2]
             self.orientation.append(theta)
-            hit, ray = self.raytrace_on_matplot(x, y, theta)
-            self.wall_hits.append(hit)
-            self.rays.append(ray)
+            
+            for t in np.linspace(-np.pi/6, np.pi/6, 30):
+                hit, ray = self.raytrace_on_matplot(x, y, theta+t)
+                if hit is not None:
+                    hit_total += 1
+                self.wall_hits.append(hit)
+                self.rays.append(ray)
+                
+        self.num_hits = hit_total
 
 
     def raytrace(self):
+        t1 = time.time()
         self.get_wall()
+        t2 = time.time()
         self.process()
+        t3 = time.time()
+        print('wall time', t2-t1)
+        print('process time', t3-t2)
         mr_arr = MarkerArray()
         
         _id = 0
@@ -221,6 +274,65 @@ class tracer():
             
         
         self.wall_hit_publisher.publish(mr_arr)
+            
+        
+        wallhit_grid = OccupancyGrid()
+        wallhit_grid.header.frame_id = 'map'
+        wallhit_grid.header.stamp = rospy.Time.now()
+        wallhit_grid.info.width = 2021
+        wallhit_grid.info.height = 2021
+        wallhit_grid.info.resolution = 0.05
+        origin = Pose()
+        origin.orientation.w = 1
+        origin.orientation.x = 0
+        origin.orientation.y = 0
+        origin.orientation.z = 0
+        
+        origin.position.x = -2021*0.05/2
+        origin.position.y = -2021*0.05/2
+        origin.position.z = 0
+    
+        wallhit_grid.info.origin = origin
+        
+        if self.num_hits < 300:
+            print('num hit', self.num_hits)
+            wallhit_grid.data = self.latest_map_origin
+            
+        else:
+            originmap = np.array(self.latest_map_origin).reshape((2021, 2021))
+            
+            center = 1010  
+            grid = -np.ones((2021, 2021))
+            
+            assert len(self.rays) == len(self.wall_hits)
+            
+            for i in range(len(self.wall_hits)):
+                hit = self.wall_hits[i]
+                ray = self.rays[i]
+                if hit is None:
+                    continue
+                for p in ray:
+                    y, x = p[0]-(2021*0.05/2), p[1]-(2021*0.05/2)
+                    
+                    grid[center + int(x/0.05)-5:center + int(x/0.05)+5, 
+                         center + int(y/0.05)-5:center + int(y/0.05)+5] = originmap[center + int(x/0.05)-5:center + int(x/0.05)+5, 
+                                                                                    center + int(y/0.05)-5:center + int(y/0.05)+5]
+                    
+                y, x = hit[0]-(2021*0.05/2), hit[1]-(2021*0.05/2)
+                grid[center + int(x/0.05)-5:center + int(x/0.05)+5, 
+                    center + int(y/0.05)-5:center + int(y/0.05)+5] = originmap[center + int(x/0.05)-5:center + int(x/0.05)+5, 
+                                                                            center + int(y/0.05)-5:center + int(y/0.05)+5]
+                
+            grid = grid.reshape((2021*2021))        
+
+            wallhit_grid.data = list(grid)
+            
+            
+        self.newMap_pub.publish(wallhit_grid)
+        
+        wallhit_map = np.zeros((2021, 2021))
+            
+        
         
         
         #im = self.latest_map.copy()
